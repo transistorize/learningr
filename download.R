7baf5ecfab51e6f3281137c7c93a6ff7ca6a46ba
Bryan Valentini <transistorize@users.noreply.github.com>
2014-04-19 23:05:07 -0400

    Update and rename download.r to download.R

--- download.R
+++ download.R
@@ -1,19 +1,17 @@
-library('XML')
 library('RCurl')
 library('stringr')
-library('plyr')
 
 # base data URL for Arsenal stats, pull from espnfc page
 team <- "Arsenal"
 baseurl <- "http://espnfc.com/team/fixtures/_/id/359/season" #2001/league/eng.1/arsenal
 seasons <- 2001:2012
 
-# fetch all the data and save it to disk
+# build all the URLs and fetch each season's web page
 mod_name <- str_replace_all(tolower(team), " ", "-")
 urls <- sapply(seasons, function(s) { return ( paste(baseurl, s,"league/eng.1", mod_name, sep="/") ); })
 raw_data <- sapply(urls, getURL, .encoding="UTF-8")
 
-# write raw data to disk
+# optional: write raw HTML data to local working directory
 io <- file("rawdata_arsenal.txt")
 cat(raw_data, file=io)
 close(io)
